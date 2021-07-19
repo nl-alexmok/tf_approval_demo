95ea38f8b9a554c3f21e28a196370f019becc3c5
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 16:57:42 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -1,184 +1,87 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "all" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
-  }
-}
-
-module "security_group" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 4.0"
-
-  name        = "example"
-  description = "Security group for example usage with EC2 instance"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-}
-
-resource "aws_eip" "this" {
-  vpc      = true
-  instance = module.ec2.id[0]
-}
-
-resource "aws_placement_group" "web" {
-  name     = "hunky-dory-pg"
-  strategy = "cluster"
-}
-
-resource "aws_kms_key" "this" {
-}
-
-resource "aws_network_interface" "this" {
-  count = 1
-
-  subnet_id = tolist(data.aws_subnet_ids.all.ids)[count.index]
-}
-
-module "ec2" {
-  source = "../../"
-
-  instance_count = 1
-
-  name          = "example-normal"
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "c5.large"
-  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
-  #  private_ips                 = ["172.31.32.5", "172.31.46.20"]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-  placement_group             = aws_placement_group.web.id
-
-  user_data_base64 = base64encode(local.user_data)
-
-  enable_volume_tags = false
-  root_block_device = [
-    {
-      volume_type = "gp2"
-      volume_size = 10
-      tags = {
-        Name = "my-root-block"
-      }
-    },
-  ]
-
-  ebs_block_device = [
-    {
-      device_name = "/dev/sdf"
-      volume_type = "gp2"
-      volume_size = 5
-      encrypted   = true
-      kms_key_id  = aws_kms_key.this.arn
-    }
-  ]
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    "Env"      = "Private"
-    "Location" = "Secret"
+    Name = "terraform-demo-main-vpc"
   }
 }
 
-module "ec2_with_t2_unlimited" {
-  source = "../../"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-  instance_count = 1
-
-  name          = "example-t2-unlimited"
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  cpu_credits   = "unlimited"
-  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
-  #  private_ip = "172.31.32.10"
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-}
-
-module "ec2_with_t3_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-t3-unlimited"
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.large"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-}
-
-module "ec2_with_metadata_options" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-metadata_options"
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.small"
-  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-
-  metadata_options = {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 8
+  tags = {
+    Name = "terraform-demo-igw"
   }
 }
 
-module "ec2_with_network_interface" {
-  source = "../../"
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
-  instance_count = 1
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-  name            = "example-network"
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = "c5.large"
-  placement_group = aws_placement_group.web.id
-
-  network_interface = [
-    {
-      device_index          = 0
-      network_interface_id  = aws_network_interface.this[0].id
-      delete_on_termination = false
-    }
-  ]
+  tags = {
+    Name = "terraform-demo-public-rt"
+  }
 }
 
-# This instance won't be created
-module "ec2_zero" {
-  source = "../../"
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
 
-  instance_count = 0
+  tags = {
+    Name = "terraform-demo-public-a"
+  }
+}
 
-  name                   = "example-zero"
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "c5.large"
-  subnet_id              = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids = [module.security_group.security_group_id]
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_instance" "demo_ec2" {
+  ami                         = "ami-09c5e030f74651050"
+  instance_type               = "t3.nano"
+  key_name                    = "alex-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_a.id
+  security_groups             = [aws_security_group.allow_ssh.id]
+
+  root_block_device {
+      encrypted   = true
+      volume_size = "10"
+  }
+
+  tags = {
+    Name = "terraform-demo-ec2"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-demo-ec2-sg"
+  }
 }
