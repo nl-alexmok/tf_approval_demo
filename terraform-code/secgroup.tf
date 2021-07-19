32aa36a400d5528402f7ad3c8da0be819d63224a
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 17:03:14 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -1,87 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "demo" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "terraform-demo-main-vpc"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "terraform-demo-igw"
-  }
-}
-
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "terraform-demo-public-rt"
-  }
-}
-
-resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "terraform-demo-public-a"
-  }
-}
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_instance" "demo_ec2" {
-  ami                         = "ami-09c5e030f74651050"
-  instance_type               = "t3.nano"
-  key_name                    = "alex-key"
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.public_a.id
-  security_groups             = [aws_security_group.allow_ssh.id]
-
-  root_block_device {
-      encrypted   = true
-      volume_size = "10"
-  }
-
-  tags = {
-    Name = "terraform-demo-ec2"
-  }
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "terraform-demo-ec2-sg"
   }
 }
