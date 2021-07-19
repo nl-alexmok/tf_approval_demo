8de1cd664835eb25a5ac99de0ad7fa1187bad449
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 17:39:52 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -1,55 +1,16 @@
-data "aws_vpc" "default" {
-  default = true
-}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-data "aws_subnet_ids" "all" {
-  vpc_id = "${data.aws_vpc.default.id}"
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
+  tags = {
+    Name = "terraform-demo-main-vpc"
   }
 }
 
-module "security_group" {
-  source = "terraform-aws-modules/security-group/aws"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.main.id}"
 
-  name        = "example"
-  description = "Security group for example usage with EC2 instance"
-  vpc_id      = "${data.aws_vpc.default.id}"
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp", "all-icmp"]
-  egress_rules        = ["all-all"]
-}
-
-resource "aws_eip" "this" {
-  vpc      = true
-  instance = "${module.ec2.id[0]}"
-}
-
-module "ec2" {
-  source = "../../"
-
-  name                        = "example"
-  ami                         = "${data.aws_ami.amazon_linux.id}"
-  instance_type               = "t2.micro"
-  subnet_id                   = "${element(data.aws_subnet_ids.all.ids, 0)}"
-  vpc_security_group_ids      = ["${module.security_group.this_security_group_id}"]
-  associate_public_ip_address = true
+  tags = {
+    Name = "terraform-demo-igw"
+  }
 }
