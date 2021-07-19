0d9e7e839afd11a490af5980cd61d6ae370ce025
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 18:08:45 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -67,3 +67,21 @@ resource "aws_security_group" "allow_ssh" {
     Name = "terraform-demo-ec2-sg"
   }
 }
+
+resource "aws_instance" "demo_ec2" {
+  ami                         = "ami-09c5e030f74651050"
+  instance_type               = "t3.nano"
+  key_name                    = "alex-key"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public_a.id
+  security_groups             = [aws_security_group.allow_ssh.id]
+
+  root_block_device {
+      encrypted   = true
+      volume_size = "10"
+  }
+
+  tags = {
+    Name = "terraform-demo-ec2"
+  }
+}
