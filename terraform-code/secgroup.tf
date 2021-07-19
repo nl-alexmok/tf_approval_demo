f0f8edace62bb3a68af6fcde2ad6af9b20791e8b
nl-alexmok <64762294+nl-alexmok@users.noreply.github.com>
2021-07-19 17:59:28 +0800

    Update secgroup.tf

--- terraform-code/secgroup.tf
+++ terraform-code/secgroup.tf
@@ -57,8 +57,8 @@ resource "aws_security_group" "allow_ssh" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 22
+    to_port     = 22
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
